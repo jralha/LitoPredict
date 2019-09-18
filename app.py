4248c6c8bf1370004035c64fae280719fba12500
jralha <42118093+jralha@users.noreply.github.com>
2019-09-18 11:38:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return upload()
+        return redirect("/predict")
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -37,14 +37,12 @@ def upload():
             data0 = df.iloc[head_lines:,:]
             data = data0.iloc[:,[col_dtc,col_dts,col_rhob,col_gr]]
 
-            #Data transformation like in model
             imp = SimpleImputer()
             polyfeat = PolynomialFeatures()
             data_imp = imp.fit_transform(data)
             data_feat = polyfeat.fit_transform(data_imp)
             data_df = pd.DataFrame(data_feat)
 
-            #Columns used in XGBoost model
             data_df.columns = ['f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'f13', 'f14']
 
             model = pickle.load(open("model.pkl",'rb'))
@@ -56,12 +54,12 @@ def upload():
 
             resp = make_response(data.to_csv())
             resp.headers["Content-Disposition"] = "attachment; filename=pred.csv"
-            #resp.headers["Content-Type"] = "text/csv"
+            resp.headers["Content-Type"] = "text/csv"
             return resp
         else:
             return render_template('upload.html')
     else:
-        return home()
+        return redirect("/login")
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
