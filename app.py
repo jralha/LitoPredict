a780eb4eb1a48913bd6def7db6503342488a1bfa
jralha <42118093+jralha@users.noreply.github.com>
2019-09-18 12:10:27 -0300

    Wrong commented lines

--- app.py
+++ app.py
@@ -62,9 +62,9 @@ def upload():
     else:
         return redirect("/login")
 
-# if __name__ == "__main__":
-     app.secret_key = os.urandom(12)
-#     app.run(debug=True)
+if __name__ == "__main__":
+    #app.secret_key = os.urandom(12)
+    app.run(debug=True)
 
 
 
