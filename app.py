37af5deba9403b57190769104037e0c48b81a541
jralha <42118093+jralha@users.noreply.github.com>
2019-09-18 12:04:29 -0300

    secret key error fix

--- app.py
+++ app.py
@@ -61,9 +61,11 @@ def upload():
     else:
         return redirect("/login")
 
-if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.secret_key = os.urandom(12)
+#     app.run(debug=True)
+app.secret_key = os.urandom(12)
+app.run(debug=True)
 
 
 
